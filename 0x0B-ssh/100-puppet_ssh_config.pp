cf8b7678edcfec235a6c4869122a19112948815a
Paul Ogolla <paulotieno2@gmail.com>
2024-05-19 14:50:47 +0300

    puppert

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,15 @@
-exec {'setup ssh':
-  command => 'echo "PasswordAuthentication no" >> /etc/ssh/sshd_config'
+# setup ssh with puppet
+exec { 'echo':
+  command => 'echo "PasswordAuthentication no" >> .etc/ssh/sshd_config'
+  path => ['/usr/bin', '/usr/sbin', '/bin']
+}
+
+exec {'restart':
   command => 'sudo service ssh restart'
+  path => ['/usr/bin', '/usr/sbin', '/bin']
+}
+
+exec {'ssh':
   command => 'ssh -A -i ~/.ssh/school ubuntu@100.27.12.25'
+  path => ['/usr/bin', '/usr//sbin/', '/bin']
 }
