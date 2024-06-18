bd1c7229644a23f161c35ecc49f78ae35b3bc8b6
Paul Ogolla <paulotieno2@gmail.com>
2024-06-18 23:58:25 +0300

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,8 +6,8 @@ import requests
 def number_of_subscribers(subreddit):
     """use reddit api"""
     r = requests.get("https://www.reddit.com/r/{}/about.json".format(subreddit),
-                      headers={"User-Agent": "Custom-User-Agent"},)
-    if r.status_code != 200:
+                      headers={"User-Agent": "Custom-User-Agent"})
+    if r.status_code >= 300:
         return 0
 
     return r.json().get('data').get('subscribers')
