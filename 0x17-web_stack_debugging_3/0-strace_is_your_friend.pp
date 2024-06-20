0275caf87efcbf055e348677843532fdc2f72c8f
Paul Ogolla <paulotieno2@gmail.com>
2024-06-21 00:33:24 +0300

    debug 3

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# command to fix issue with server
+exec { 'patch':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
+}
